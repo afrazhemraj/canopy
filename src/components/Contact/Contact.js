a7438d0fb71e246d4842a3ca469364e7e3db1956
afrazhemraj <afrazhemraj@gmail.com>
2024-03-01 21:01:20 -0500

    feature: emailjs functionality

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,12 +1,44 @@
 import "./Contact.scss";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const refForm = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_csq4t6k",
+        "template_wi1lrsq",
+        refForm.current,
+        "I_tMrRkNF_LkmtGRk"
+      )
+      .then(
+        () => {
+          alert("Message successfully sent!");
+          window.location.reload(false);
+        },
+        () => {
+          alert("Failed to send the message, please try again");
+        }
+      );
+  };
+
   return (
     <div className="contact">
       <div className="contact-content">
         <h1>Curious? Let’s Connect.</h1>
         <p>we’re excited to hear from you.</p>
-        <div className="contact-button">Submit</div>
+        <div className="contact-form">
+          <form ref={refForm} onSubmit={sendEmail}>
+            <input type="text" name="name" placeholder="your name" />
+            <input type="email" name="email" placeholder="email" />
+            <textarea name="message" placeholder="message" />
+            <button className="contact-button" type="submit">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
